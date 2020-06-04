5b0f1bce8ad916288d3d1c112111fb4a5789a06b
SlevinWasAlreadyTaken <nicolas.sadin@misakey.com>
2020-06-04 15:56:11 +0200

    fix(sso): take Complete attribute into account to consider emailed code conflict

--- api/src/modules/sso/application/authn/emailed_code.go
+++ api/src/modules/sso/application/authn/emailed_code.go
@@ -18,9 +18,11 @@ func (as *Service) CreateEmailedCode(ctx context.Context, identityID string) err
 	if err != nil && !merror.HasCode(err, merror.NotFoundCode) {
 		return err
 	}
-	if err == nil && time.Now().Sub(existing.CreatedAt) < as.codeValidity {
+	// if the last authn step is not complete and not expired, we can't create a new one
+	if err == nil && !existing.Complete &&
+		time.Now().Sub(existing.CreatedAt) < as.codeValidity {
 		return merror.Conflict().
-			Describe("a code has already been generated").
+			Describe("a code has already been generated and not used").
 			Detail("identity_id", merror.DVConflict).
 			Detail("method_name", merror.DVConflict)
 	}
