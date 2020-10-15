b6c7609552e18d0d6f4feb3e283ff9e0b2ba0bf6
SlevinWasAlreadyTaken <nicolas.sadin@misakey.com>
2020-10-15 14:55:52 +0200

    box: close redis pubsub conn when listener ends

--- api/src/modules/box/entrypoints/ws_handler.go
+++ api/src/modules/box/entrypoints/ws_handler.go
@@ -1,6 +1,8 @@
 package entrypoints
 
 import (
+	"context"
+
 	"github.com/go-redis/redis/v7"
 	"github.com/labstack/echo/v4"
 
@@ -28,6 +30,7 @@ func (wh *WebsocketHandler) RedisListener(
 	chName string,
 	handler func(echo.Context, WebsocketHandler, []byte) error,
 ) error {
+	ctx := c.Request().Context()
 	ws, err := mwebsockets.NewWebsocket(c, wh.allowedOrigins, wsName)
 	if err != nil {
 		return err
@@ -36,6 +39,12 @@ func (wh *WebsocketHandler) RedisListener(
 	go ws.Pump(c)
 
 	sub := wh.boxService.RedConn.Subscribe(chName)
+	// plan subscribe closing
+	defer func(ctx context.Context, sub *redis.PubSub) {
+		if err := sub.Close(); err != nil {
+			logger.FromCtx(ctx).Error().Msgf("could not close sub: %v", err)
+		}
+	}(ctx, sub)
 
 	// we instantiate this interface to wait for
 	// the subscription to be active
@@ -48,30 +57,29 @@ func (wh *WebsocketHandler) RedisListener(
 	// taken on sub since it was created.
 	switch msg := iface.(type) {
 	case *redis.Subscription:
-		logger.FromCtx(c.Request().Context()).Debug().Msg("Redis Subscription successful")
+		logger.FromCtx(ctx).Debug().Msg("Redis Subscription successful")
 	case *redis.Message:
 		ws.Send <- mwebsockets.WebsocketMessage{
 			Msg: msg.Payload,
 		}
 	default:
-		logger.FromCtx(c.Request().Context()).Debug().Msgf("Unexpected redis notification type: %s", msg)
+		logger.FromCtx(ctx).Debug().Msgf("Unexpected redis notification type: %s", msg)
 	}
 
-	ch := sub.Channel()
-
-	logger.FromCtx(c.Request().Context()).Debug().Msg("Websocket loop started")
+	logger.FromCtx(ctx).Debug().Msg("Websocket loop started")
+	subNotif := sub.Channel()
 	for {
 		select {
 		case <-ws.EndPump:
 			return nil
 		case msg := <-ws.Handler:
 			if err := handler(c, *wh, msg); err != nil {
-				logger.FromCtx(c.Request().Context()).Warn().Err(err).Msgf("%s: unable to handle msg", ws.ID)
+				logger.FromCtx(ctx).Warn().Err(err).Msgf("%s: unable to handle msg", ws.ID)
 			}
-		case msg, ok := <-ch:
+		case msg, ok := <-subNotif:
 			if !ok {
 				// here we close the websocket and send an error to the client
-				logger.FromCtx(c.Request().Context()).Error().Msg("cannot receive redis messages")
+				logger.FromCtx(ctx).Error().Msg("cannot receive redis messages")
 				_ = ws.SendCloseMessage()
 				return merror.Internal().Describe("cannot receive redis messages")
 			}
